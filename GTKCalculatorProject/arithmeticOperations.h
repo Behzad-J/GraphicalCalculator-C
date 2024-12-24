b4e10841b0e49cfc77b20ee456a5739f7d0b39e8
Behzad Janjua <156612833+Behzad-J@users.noreply.github.com>
2024-12-24 00:56:54 -0500

    Delete GTKCalculatorProject directory

--- GTKCalculatorProject/arithmeticOperations.h
+++
@@ -1,4 +0,0 @@
-double addition(double num1, double num2);
-double subtraction(double num1, double num2);
-double multiplication(double num1, double num2);
-double division(double num1, double num2);
--- GTKCalculatorProject/evaluateWithParentheses.h
+++
@@ -1,2 +0,0 @@
-char* adjustValues(char* data, int *size);
-char* evaluateWithParentheses(char* data, int *dataSize);
